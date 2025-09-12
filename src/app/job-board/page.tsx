f859199277a423b4c5b2f533da6fe7aee3750508
Suman Chandra Barman <sumanchandrabarman999@gmail.com>
2025-09-12 15:23:24 +0600

    Change text

--- src/app/job-board/page.tsx
+++ src/app/job-board/page.tsx
@@ -133,10 +133,10 @@ function JobBoardPage() {
       </div>
       {/* Jobs */}
       <div className="container mx-auto px-4 md:px-0">
-        {/* New Listing Jobs */}
+        {/* New Jobs */}
         <div>
-          <h2 className="text-3xl md:text-4xl text-red-400 font-semibold my-10">
-            New Listing
+          <h2 className="text-2xl md:text-4xl text-red-400 font-semibold my-10">
+            New Jobs
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 md:gap-4 mb-8">
             {jobPosts.slice(0, 4).map((job) => (
@@ -146,7 +146,7 @@ function JobBoardPage() {
         </div>
         {/* All Jobs */}
         <div>
-          <h2 className="text-3xl md:text-4xl font-semibold my-10">All Jobs</h2>
+          <h2 className="text-2xl md:text-4xl font-semibold my-10">All Jobs</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 md:gap-4 mb-8">
             {jobPosts.map((job) => (
               <JobCard key={job.id} job={job} />
